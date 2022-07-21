e85df7b6a40caac99741f2a3866f3b53349a0dbd
rbernanda <acappemenang@gmail.com>
2022-07-22 04:24:08 +0700

    add shared data

--- apps/host/src/app/app.tsx
+++ apps/host/src/app/app.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import { Navbar, Footer } from '@nx-faster-build--example/shared-ui'
+import { PATH_AUTH, PATH_HOME, PATH_PROFILE } from '@nx-faster-build--example/data'
 
 const Auth = React.lazy(() => import('auth/Module'));
 const Profile = React.lazy(() => import('profile/Module'));
@@ -17,9 +18,9 @@ export function App() {
       <BaseContainer>
         <Navbar />
         <Routes>
-          <Route path={'/'} element={<Home />} />
-          <Route path={'/auth'} element={<Auth />} />
-          <Route path={'/profile'} element={<Profile />} />
+          <Route path={PATH_HOME} element={<Home />} />
+          <Route path={PATH_AUTH} element={<Auth />} />
+          <Route path={PATH_PROFILE} element={<Profile />} />
         </Routes>
         <Footer />
       </BaseContainer>
---
+++ libs/data/src/lib/pathnames.ts
@@ -0,0 +1,3 @@
+export const PATH_HOME = '/'
+export const PATH_AUTH = '/auth'
+export const PATH_PROFILE = '/profile'
